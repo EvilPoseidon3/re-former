65e943e019997eeb68f3559e6582347cbef29bb0
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-10-03 12:25:45 -0500

    finished project final commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
+  def index
+    @index = User.all
+  end
+
   def new
     @user = User.new
   end
 
 
   def create
-      # @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
-      @user = User.new(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
@@ -16,5 +20,19 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :password, :email)
+  end
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 end
